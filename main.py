d7c9daf628338b9cb7c24548bfcad95e60a394c0
yuxiaota@gmail.com <Yuxiao Tan>
2018-11-18 03:17:59 +0000

    uncomment the tests

--- main.py
+++ main.py
@@ -34,7 +34,15 @@ def load_vgg(sess, vgg_path):
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
     
-    return None, None, None, None, None
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    graph = tf.get_default_graph()
+    w1 = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    layer3_out = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    layer4_out = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    layer7_out = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
+    
+    return w1, keep, layer3_out, layer4_out, layer7_out
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -48,7 +56,32 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
-    return None
+    # encoder
+    conv_1x1_output = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding="same",
+                               #kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+                               kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    # Upscaling
+    output = tf.layers.conv2d_transpose(conv_1x1_output, num_classes, 4, 2, padding="same",
+                                        #kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    #add skipp connection
+    skip_conn_1_input = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, padding="same",
+                                        #kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    skip_conn_1_output = tf.add(output, skip_conn_1_input)
+    output = tf.layers.conv2d_transpose(skip_conn_1_output, num_classes, 4, 2, padding="same",
+                                        #kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    #add skipp connection
+    skip_conn_2_input = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, padding="same",
+                                         #kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+                                         kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    skip_conn_2_output = tf.add(output, skip_conn_2_input)
+    output = tf.layers.conv2d_transpose(skip_conn_2_output, num_classes, 16, 8, padding="same",
+                                        #ernel_initializer= tf.random_normal_initializer(stddev=0.01),
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    return output
+    
 tests.test_layers(layers)
 
 
@@ -62,7 +95,12 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
-    return None, None, None
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
+    optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate)
+    train_op = optimizer.minimize(cross_entropy_loss)
+
+    return logits, train_op, cross_entropy_loss
 tests.test_optimize(optimize)
 
 
@@ -82,14 +120,21 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param learning_rate: TF Placeholder for learning rate
     """
     # TODO: Implement function
-    pass
+    for epoch in range(epochs):
+        for image, label in get_batches_fn(batch_size):
+#             logits, train_op, cross_entropy_loss = optimize(nn_last_layer, correct_label, learning_rate, num_classes)
+                _,loss = sess.run([train_op,cross_entropy_loss], 
+                                  feed_dict={input_image: image, correct_label: label, keep_prob:0.5, learning_rate: 0.0001})
+        print("EPOCH {} ...".format(epoch + 1))
+        print("Loss: = {:.3f}".format(loss))
+        
 tests.test_train_nn(train_nn)
 
 
 def run():
     num_classes = 2
     image_shape = (160, 576)
-    data_dir = './data'
+    data_dir = '/data'
     runs_dir = './runs'
     tests.test_for_kitti_dataset(data_dir)
 
@@ -108,14 +153,25 @@ def run():
 
         # OPTIONAL: Augment Images for better results
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
-
+        
         # TODO: Build NN using load_vgg, layers, and optimize function
-
+        correct_label = tf.placeholder(tf.float32, name='correct_label')
+        learning_rate = tf.placeholder(tf.float32, name='learning_rate')
+        
+        input_image, keep_prob, vgg_layer3_out, vgg_layer4_out, vgg_layer7_out = load_vgg(sess, vgg_path)
+        nn_output = layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes)
+        logits, train_op, cross_entropy_loss = optimize(nn_output, correct_label, learning_rate, num_classes)
         # TODO: Train NN using the train_nn function
-
+        epochs = 50
+        batch_size = 5
+        # initialized variables before training
+        sess.run(tf.global_variables_initializer())
+        
+        train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
+             correct_label, keep_prob, learning_rate)
         # TODO: Save inference data using helper.save_inference_samples
-        #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
-
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+        
         # OPTIONAL: Apply the trained model to a video
 
 
